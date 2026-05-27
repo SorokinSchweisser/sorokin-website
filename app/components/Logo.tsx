0cf73e718b63c4c7273a496f84c1d644c142f220
Pablo Gela <pablo@MacBook-Air-von-Pablo.fritz.box>
2026-05-27 14:04:29 +0200

    fix: use img tag instead of next/image

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type LogoProps = {
   size?: "sm" | "md" | "lg";
   variant?: "light" | "dark";
@@ -8,12 +6,9 @@ type LogoProps = {
 export default function Logo({ size, variant }: LogoProps) {
   return (
     <a href="#" className="flex items-center">
-      <Image
+      <img
         src="/logo.png"
         alt="SOROKIN Mobiler Schweißservice"
-        width={200}
-        height={80}
-        priority
         className="h-12 md:h-16 w-auto object-contain"
       />
     </a>
